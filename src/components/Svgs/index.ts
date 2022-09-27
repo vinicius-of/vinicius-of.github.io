b1cf34def46e6a4eff78a2be0205947a35acfc06
Vinícius de Oliveira Fonseca <dev.vinicius.oliveirafonseca@gmail.com>
2022-09-27 00:12:54 -0300

    Creating SVG components to the website

---
+++ src/components/Svgs/index.ts
@@ -0,0 +1,15 @@
+import ReactLogo from "./ReactLogo";
+import PostgresLogo from './PostgresLogo';
+import NodejsLogo from "./NodejsLogo";
+import NestjsLogo from "./NestjsLogo";
+import TypescriptLogo from "./TypescriptLogo";
+import GitLogo from "./GitLogo";
+import AboutPortfolioIcon from "./AboutPortfolioIcon";
+import ContactIcon from "./ContactIcon";
+import HomeIcon from "./HomeIcon";
+import ProjectsIcon from "./ProjectsIcon";
+import GithubIcon from "./GithubIcon";
+import LinkedinIcon from "./LinkedinIcon";
+import ProfileIcon from "./ProfileIcon";
+
+export { ReactLogo, PostgresLogo, NodejsLogo, NestjsLogo, TypescriptLogo, GitLogo, AboutPortfolioIcon, ContactIcon, HomeIcon, ProjectsIcon, GithubIcon, LinkedinIcon, ProfileIcon };
