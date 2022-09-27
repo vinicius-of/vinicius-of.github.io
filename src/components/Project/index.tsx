989821ae8580a11d247746e6d89139441df00172
Vinícius de Oliveira Fonseca <dev.vinicius.oliveirafonseca@gmail.com>
2022-09-27 00:13:52 -0300

    Component Project created

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,28 +1,28 @@
 import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
 import React, { FC } from 'react';
 //@ts-ignore
-import * as styles from './project.module.css';
+import { projectItem, roleStyle } from './project.module.css';
 
 type ProjectProps = {
     title: string;
     imageData: IGatsbyImageData;
     description?: string;
+    alt: string;
+    role?: string;
 };
 
 export const Project: FC<ProjectProps> = ({
     title,
     description,
     imageData,
+    alt,
+    role,
 }) => {
     return (
-        <div className={styles.projectItem}>
-            <GatsbyImage
-                image={imageData}
-                alt="Teste"
-                imgClassName={styles.image}
-                className={styles.containerImage}
-            />
+        <div className={projectItem}>
+            <GatsbyImage image={imageData} alt={alt} />
             <h3>{title}</h3>
+            <span className={roleStyle}>{role}</span>
             <p>{description}</p>
         </div>
     );
