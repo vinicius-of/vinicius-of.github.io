0c3ca1239900707693f15813fa987832baf7c0a4
Vinícius de Oliveira Fonseca <dev.vinicius.oliveirafonseca@gmail.com>
2022-09-27 00:15:08 -0300

    Main page created and modified

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,39 +2,99 @@ import * as React from 'react';
 import { graphql, HeadFC, useStaticQuery } from 'gatsby';
 import Navbar from '../components/Navbar';
 import Project from '../components/Project';
+import Article from '../components/Article';
 import '../global.css';
+//@ts-ignore
+import { descriptions, mainContainer } from '../styles/index.module.css';
+import Introduction from '../components/Introduction';
+import Contact from '../components/Contact';
 
 const IndexPage = () => {
     const query = useStaticQuery(graphql`
-        query Images {
-            allImageSharp {
+        query Experiences {
+            allMarkdownRemark {
                 edges {
                     node {
-                        gatsbyImageData
                         id
+                        frontmatter {
+                            title
+                            imageAlt
+                            role
+                            content
+                            image {
+                                childImageSharp {
+                                    gatsbyImageData(placeholder: DOMINANT_COLOR)
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     `);
 
-    const imagesData = [...query.allImageSharp.edges];
+    const experiences = query.allMarkdownRemark.edges;
 
     return (
-        <div>
+        <>
             <Navbar />
-            <br />
-            {imagesData.map((data: any) => {
-                return (
-                    <Project
-                        key={data.node.id}
-                        title="Netflix Clone"
-                        imageData={data.node.gatsbyImageData}
-                        description="Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker."
-                    />
-                );
-            })}
-        </div>
+            <header id="home">
+                <Introduction />
+            </header>
+            <main className={mainContainer}>
+                <section id="about" className={descriptions}>
+                    <Article title="Sobre meu perfil">
+                        Eu me considero ser uma pessoa organizada e competente.
+                        Tento sempre me desafiar, mesmo que eu não consiga ver a
+                        solução inicialmente. Sempre estou disposto a ajudar um
+                        colega de equipe quando disponível. Eu acredito na
+                        prioridade da qualidade e do planejamento de um
+                        produto/ambiente antes de quantidade, eu vejo que isso
+                        pode trazer um valor bem maior quando cultivado e bem
+                        cuidado para o futuro.
+                    </Article>
+                    <Article title="Foco em qualidade">
+                        Sempre tive um foco em qualidade, em manter o código
+                        limpo (Clean code), de manter-se dentro da arquitetura e
+                        dos padrões do projeto estimado anteriormente. Acredito
+                        que para um projeto funcionar precisa não somente de
+                        fazer o código funcionar, mas também ser sustentável a
+                        longo prazo.
+                    </Article>
+                    <Article title="Organização é importante">
+                        O futuro não é previsível, nunca sabemos o que pode
+                        acontecer. Flexibilidade é importante em momentos como
+                        este e para ser flexível, temos que ser organizado.
+                        Gosto de ser cuidadoso, ás vezes retornando um passo
+                        para avançar dois.
+                    </Article>
+                </section>
+                <section id="projects">
+                    {experiences.map((data: any) => {
+                        const { title, image, imageAlt, content, role } =
+                            data.node.frontmatter;
+
+                        console.log(role);
+
+                        return (
+                            <Project
+                                key={data.node.id}
+                                title={title}
+                                alt={imageAlt}
+                                imageData={
+                                    image.childImageSharp.gatsbyImageData
+                                }
+                                description={content}
+                                role={role}
+                            />
+                        );
+                    })}
+                </section>
+            </main>
+            <footer id="contact">
+                <Contact />
+            </footer>
+        </>
     );
 };
 
