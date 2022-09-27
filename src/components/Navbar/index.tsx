9e8e021f2b534a9095f30dda76c4aa393eac24c6
Vinícius de Oliveira Fonseca <dev.vinicius.oliveirafonseca@gmail.com>
2022-09-27 00:13:41 -0300

    Component Navbar created

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import './navbar.module.css';
+import React, { useEffect, useState } from 'react';
+import {
+    AboutPortfolioIcon,
+    ContactIcon,
+    HomeIcon,
+    ProjectsIcon,
+} from '../Svgs';
+import ProfileIcon from '../Svgs/ProfileIcon';
+//@ts-ignore
+import { hide, navbar } from './navbar.module.css';
 
 export const Navbar = () => {
+    const [classes, setClasses] = useState<string>(hide);
+    const limitToHide = 270;
+
+    const handleDisplay = () => {
+        const isOutOfIntroduction = window.scrollY > limitToHide;
+
+        if (!isOutOfIntroduction) {
+            setClasses(hide);
+        } else {
+            setClasses(navbar);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleDisplay);
+
+        return () => {
+            window.removeEventListener('scroll', handleDisplay);
+        };
+    }, []);
+
     return (
-        <nav>
-            <a href="">Sobre</a>
-            <a href="">Currículo</a>
-            <a href="">Links</a>
-            <a href="">Projetos</a>
-            <a href="">Contato</a>
+        <nav className={classes}>
+            <a href="#home">
+                <HomeIcon />
+                <span>Introdução</span>
+            </a>
+            <a href="#about">
+                <ProfileIcon />
+                <span>Sobre mim</span>
+            </a>
+            <a href="#projects">
+                <ProjectsIcon />
+                <span>Experiências</span>
+            </a>
+            <a href="#contact">
+                <ContactIcon />
+                <span>Contato</span>
+            </a>
         </nav>
     );
 };
