d191d05cc2e4ac9a68324fa8dc0ae0cd3e7d4478
Vinicius <vinicius.oliveirafonseca@gmail.com>
2022-10-18 19:09:25 -0300

    Finished main page index.tsx and responsivity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,19 @@ import { graphql, HeadFC, useStaticQuery } from 'gatsby';
 import Navbar from '../components/Navbar';
 import Project from '../components/Project';
 import Article from '../components/Article';
+import Introduction from '../components/Introduction';
+import Contact from '../components/Contact';
+import Columns from '../components/Columns';
 import '../global.css';
 import {
     descriptions,
     mainContainer,
     listExperiences,
     title,
+    about,
     //@ts-ignore
 } from '../styles/index.module.css';
-import Introduction from '../components/Introduction';
-import Contact from '../components/Contact';
+import FastContact from '../components/FastContact';
 
 const IndexPage = () => {
     const query = useStaticQuery(graphql`
@@ -25,6 +28,7 @@ const IndexPage = () => {
                             title
                             imageAlt
                             role
+                            stack
                             content
                             image {
                                 childImageSharp {
@@ -47,39 +51,52 @@ const IndexPage = () => {
                 <Introduction />
             </header>
             <main className={mainContainer}>
-                <section id="about" className={descriptions}>
-                    <Article title="Sobre meu perfil">
-                        Eu me considero ser uma pessoa organizada e competente.
-                        Tento sempre me desafiar, mesmo que eu não consiga ver a
-                        solução inicialmente. Sempre estou disposto a ajudar um
-                        colega de equipe quando disponível. Eu acredito na
-                        prioridade da qualidade e do planejamento de um
-                        produto/ambiente antes de quantidade, eu vejo que isso
-                        pode trazer um valor bem maior quando cultivado e bem
-                        cuidado para o futuro.
-                    </Article>
-                    <Article title="Foco em qualidade">
-                        Sempre tive um foco em qualidade, em manter o código
-                        limpo (Clean code), de manter-se dentro da arquitetura e
-                        dos padrões do projeto estimado anteriormente. Acredito
-                        que para um projeto funcionar precisa não somente de
-                        fazer o código funcionar, mas também ser sustentável a
-                        longo prazo.
-                    </Article>
-                    <Article title="Organização é importante">
-                        O futuro não é previsível, nunca sabemos o que pode
-                        acontecer. Flexibilidade é importante em momentos como
-                        este e para ser flexível, temos que ser organizado.
-                        Gosto de ser cuidadoso, ás vezes retornando um passo
-                        para avançar dois.
-                    </Article>
+                <FastContact />
+                <section id="about" className={about}>
+                    <Columns>
+                        <div className={descriptions}>
+                            <Article title="Sobre meu perfil">
+                                Eu me considero ser uma pessoa organizada e
+                                competente. Tento sempre me desafiar, mesmo que
+                                eu não consiga ver a solução inicialmente.
+                                Sempre estou disposto a ajudar um colega de
+                                equipe quando disponível. Eu acredito na
+                                prioridade da qualidade e do planejamento de um
+                                produto/ambiente antes de quantidade, eu vejo
+                                que isso pode trazer um valor bem maior quando
+                                cultivado e bem cuidado para o futuro.
+                            </Article>
+                            <Article title="Foco em qualidade">
+                                Sempre tive um foco em qualidade, em manter o
+                                código limpo (Clean code), de manter-se dentro
+                                da arquitetura e dos padrões do projeto estimado
+                                anteriormente. Acredito que para um projeto
+                                funcionar precisa não somente de fazer o código
+                                funcionar, mas também ser sustentável a longo
+                                prazo.
+                            </Article>
+                            <Article title="Organização é importante">
+                                O futuro não é previsível, nunca sabemos o que
+                                pode acontecer. Flexibilidade é importante em
+                                momentos como este e para ser flexível, temos
+                                que ser organizado. Gosto de ser cuidadoso, ás
+                                vezes retornando um passo para avançar dois.
+                            </Article>
+                        </div>
+                    </Columns>
                 </section>
                 <section id="experiences">
                     <h2 className={title}>Minhas experiências</h2>
                     <div className={listExperiences}>
                         {experiences.map((data: any) => {
-                            const { title, image, imageAlt, content, role } =
-                                data.node.frontmatter;
+                            const {
+                                title,
+                                image,
+                                imageAlt,
+                                content,
+                                role,
+                                stack,
+                            } = data.node.frontmatter;
 
                             return (
                                 <Project
@@ -91,6 +108,7 @@ const IndexPage = () => {
                                     }
                                     description={content}
                                     role={role}
+                                    stack={stack}
                                 />
                             );
                         })}
